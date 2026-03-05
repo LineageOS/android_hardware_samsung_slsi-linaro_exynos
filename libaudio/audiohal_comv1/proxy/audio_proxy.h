d6011ea9eba69631564e66c812dc191b3ef70ea6
ExtremeXT <extremext@lineageos.org>
2026-03-05 19:39:27 +0200

    exynos: audiohal_comv1: proxy: Move platform-specific configuration to device trees
    Change-Id: I234980a60b0083ddff194591e53dcd7a19c32d3f
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- libaudio/audiohal_comv1/proxy/audio_proxy.h
+++ libaudio/audiohal_comv1/proxy/audio_proxy.h
@@ -33,9 +33,9 @@
 #include "audio_devices.h"
 #include "audio_offload.h"
 
-#include "audio_pcm.h"
-#include "audio_mixer.h"
-#include "audio_abox.h"
+#include <audio_pcm.h>
+#include <audio_mixer.h>
+#include <audio_abox.h>
 #include "audio_streamconfig.h"
 #include "audio_board_info.h"
 
