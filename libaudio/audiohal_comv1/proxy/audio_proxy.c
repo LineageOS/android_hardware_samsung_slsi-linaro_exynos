368358cac09eee873bedf7d8b9b49351431f0b2c
Secondarian <robertanitei@tutanota.com>
2026-04-01 15:49:16 +0200

    exynos: audiohal_comv1: proxy: Fix Compressed Volume
    * Required when building AudioHAL as 64-bit.
    
    Change-Id: I631d8f201d1753a6d208b4a990320a2f9612cde8
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- libaudio/audiohal_comv1/proxy/audio_proxy.c
+++ libaudio/audiohal_comv1/proxy/audio_proxy.c
@@ -5412,7 +5412,10 @@ void proxy_set_volume(void *proxy, int volume_type, float left, float right)
     if (ctrl) {
         switch(volume_type) {
             case VOLUME_TYPE_OFFLOAD:
-                ret = mixer_ctl_set_array(ctrl, val, sizeof(val)/sizeof(val[0]));
+                for (int i = 0; i < sizeof(val)/sizeof(val[0]); i++) {
+                    ret = mixer_ctl_set_value(ctrl, i, val[i]);
+                    if (ret) break;
+                }
                 break;
             case VOLUME_TYPE_MMAP:
             case VOLUME_TYPE_CALL:
