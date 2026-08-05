302e3b98a0a7c5fb68487e95a911cef20bf5b83e
Flopster101 <nahuelgomez329@gmail.com>
2026-08-05 20:27:08 -0300

    exynos: audiohal_comv1: Add support for dedicated VOIP_RX stream
    Support handling the AUDIO_OUTPUT_FLAG_VOIP_RX flag by mapping it to a
    dedicated playback stream (ASTREAM_PLAYBACK_VOIP_RX) and routing it to
    its own PCM device (VOIP_RX_PLAYBACK_DEVICE).
    
    This allows VoIP playback to route through a separate PCM channel instead of sharing the primary output stream with media playback.
    
    Change-Id: Ibf09395321073f492e0c5e05c44d55d6397dfa7d
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- libaudio/audiohal_comv1/proxy/audio_proxy.c
+++ libaudio/audiohal_comv1/proxy/audio_proxy.c
@@ -389,6 +389,10 @@ static int get_pcm_device_number(void *proxy, void *proxy_stream)
                 pcm_device_number = LOW_PLAYBACK_DEVICE;
                 break;
 
+            case ASTREAM_PLAYBACK_VOIP_RX:
+                pcm_device_number = VOIP_RX_PLAYBACK_DEVICE;
+                break;
+
             case ASTREAM_PLAYBACK_DEEP_BUFFER:
                 pcm_device_number = ((apstream->pcmconfig.rate > DEFAULT_MEDIA_SAMPLING_RATE) ?
                                     DEEP_PLAYBACK_DIRECT_DEVICE : DEEP_PLAYBACK_DEVICE);
@@ -3033,6 +3037,12 @@ void *proxy_create_playback_stream(void *proxy, int type, void *config, char *ad
             apstream->sound_card = LOW_PLAYBACK_CARD;
             apstream->sound_device = get_pcm_device_number(aproxy, apstream);
             apstream->pcmconfig = pcm_config_low_playback;
+            break;
+
+        case ASTREAM_PLAYBACK_VOIP_RX:
+            apstream->sound_card = VOIP_RX_PLAYBACK_CARD;
+            apstream->sound_device = get_pcm_device_number(aproxy, apstream);
+            apstream->pcmconfig = pcm_config_voip_rx_playback;
             break;
 
         case ASTREAM_PLAYBACK_COMPR_OFFLOAD:
