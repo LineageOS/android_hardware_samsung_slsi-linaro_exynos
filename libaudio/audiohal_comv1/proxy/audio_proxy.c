8dcb10f28f5d123a3e958c1764a819b33b14df4b
Secondarian <robertanitei@tutanota.com>
2026-08-15 15:33:57 +0300

    exynos: audiohal_comv1: proxy: Use default USB paths on platforms with ASYNC support
    Change-Id: Ic9b0768bcc77c70b5b608a1ee39a37458042c258
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- libaudio/audiohal_comv1/proxy/audio_proxy.c
+++ libaudio/audiohal_comv1/proxy/audio_proxy.c
@@ -1817,6 +1817,7 @@ static void add_usb_path_extn(
     char tempStr[MAX_PATH_NAME_LEN] = {0};
     char* szDump = NULL;
 
+#ifndef ABOX_PACKED_UDMA_WR0_NAME
     /* check whether routing is for USB Headset Out or In
      * USB-IN: default path is direct i.e usb_incom -> VPCMIN_DAI
      * USB_out: default path is loop i.e BDMixer -> SIFS0 -> WDMA3-> usb_outcom
@@ -1857,6 +1858,13 @@ static void add_usb_path_extn(
             ALOGI("proxy-%s: path: %s", __func__, path_name);
         }
     }
+#else /* ifndef ABOX_PACKED_UDMA_WR0_NAME */
+    char tempRet[MAX_PATH_NAME_LEN] = {0};
+    strncpy(tempStr, path_name, szDump - path_name);
+    sprintf(tempRet, "%s%s%s", tempStr, "async-", szDump);
+    strncpy(path_name, tempRet, MAX_PATH_NAME_LEN);
+    ALOGI("proxy-%s: path: %s", __func__, path_name);
+#endif /* ifndef ABOX_PACKED_UDMA_WR0_NAME */
 #endif /* SUPPORT_USB_OFFLOAD */
 
     return;
