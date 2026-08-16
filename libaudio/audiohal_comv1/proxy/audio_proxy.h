759e0dc717ddbe3e367e5687862d44ebf9680055
Secondarian <robertanitei@tutanota.com>
2026-08-16 17:10:35 +0200

    exynos: audiohal_comv1: proxy: Enable VTX Ref Node if Present
    This is required on platforms such as erd9945 and erd8845 or newer for
    input to be registered during CP calls.
    
    Change-Id: I7aede8b4d1b7aadd93d24c1419b3169c3290f48b
    Signed-off-by: Secondarian <robertanitei@tutanota.com>

--- libaudio/audiohal_comv1/proxy/audio_proxy.h
+++ libaudio/audiohal_comv1/proxy/audio_proxy.h
@@ -194,9 +194,12 @@ struct audio_proxy
 #endif
 
     /* PCM Devices for Voice Call */
-    struct pcm *call_rx;    // CP to Output Devices
-    struct pcm *call_tx;    // Input Devices to CP
+    struct pcm *call_rx;           // CP to Output Devices
+    struct pcm *call_tx;           // Input Devices to CP
     struct pcm *call_tx_direct;    // Direct routing for Input Devices
+#ifdef VTX_REF_CAPTURE_DEVICE
+    struct pcm *call_tx_ref;       // Reference Input to CP
+#endif
 
     // Call State
     bool call_state;
